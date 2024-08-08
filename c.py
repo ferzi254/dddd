7e4d98a9216584d8f2db27f2dfa4b2672e9565e6
Ferzi <ferzeli.f@gmail.com>
2024-08-08 23:31:14 +0400

    1-ci ders

---
+++ c.py
@@ -0,0 +1,13 @@
+def yuma():
+ palatyuyan=input("xahis edirik rejim secin")
+ match palatyuyan:
+  case "120d":
+       print("paltar 120d yu")
+  case "130d":
+        print("130d yu")
+  case _:
+       print("error")
+       
+yuma()
+
+    
